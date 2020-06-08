9362e5b165d3ddde2456801a17329de15abb88b2
Ralph Baines <ralph.baines@gmail.com>
2020-06-08 07:51:45 +0100

    Change to Hosted URL

--- src/_helpers/apiConf.tsx
+++ src/_helpers/apiConf.tsx
@@ -1,7 +1,7 @@
 const ApiConf = {
-    urlofffffffff: 'https://7k7zi7zooe.execute-api.eu-west-2.amazonaws.com/dev/',
+    url: 'https://7k7zi7zooe.execute-api.eu-west-2.amazonaws.com/dev/',
     proxyurl: "https://cors-anywhere.herokuapp.com/",
-    url: 'http://localhost:3000/dev/'
+    urlxxxxxxx: 'http://localhost:3000/dev/'
 }
 
 export default ApiConf
